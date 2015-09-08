f89d8d6c55b40e52052af6ef02ea2f602f7b0c9c
Shan1024 <gambit1024@gmail.com>
2015-09-08 18:27:17 +0530

    Test class for word suggession added

--- src/WordPredictor.java
+++ src/WordPredictor.java
@@ -1,6 +1,9 @@
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.TextArea;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Locale;
 import javax.swing.JFrame;
 
 /*
@@ -22,19 +25,44 @@ public class WordPredictor extends JFrame {
 
         this.setTitle("සිංහල වචන අනුමාන කරණය");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         textArea = new TextArea();
         textArea.setPreferredSize(new Dimension(600, 400));
         textArea.setFont(SINHALA_FONT);
 
+        textArea.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+                super.keyTyped(e); //To change body of generated methods, choose Tools | Templates.
+                System.out.println("OK: " + toUnicode(e.getKeyChar()));
+                if (e.getKeyChar() == KeyEvent.VK_SPACE) {
+                    System.out.println("Space detected");
+                } else if (e.getKeyChar() == KeyEvent.VK_PERIOD) {
+                    System.out.println("Period Detected");
+                }
+            }
+
+        });
+
         this.add(textArea);
+
+        Locale loc = new Locale("si", "LK");//Language, Country
+        textArea.setLocale(loc);
+        textArea.getInputContext().selectInputMethod(loc);
 
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
+    private static String toUnicode(char ch) {
+        return String.format("\\u%04x", (int) ch);
+    }
+
     public static void main(String[] args) {
         WordPredictor wordPredictor = new WordPredictor();
     }
+
 }
